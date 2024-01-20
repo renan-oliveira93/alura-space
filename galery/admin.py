674195c68313367e334db8068982af613b5ee60c
Renan de Oliveira <renan_oliveira93@outlook.com.br>
2024-01-20 19:42:26 -0300

    feat: create admin

--- galery/admin.py
+++ galery/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from galery.models import Picture
 
-# Register your models here.
+class picture_list(admin.ModelAdmin):
+    list_display = ("id", "name", "credits")
+    list_display_links =("id", "name")
+    search_fields = ("name",)
+
+admin.site.register(Picture, picture_list)
