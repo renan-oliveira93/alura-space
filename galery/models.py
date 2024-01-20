810d86145191a861123e38bbf8bc36f61ec9b390
Renan de Oliveira <renan_oliveira93@outlook.com.br>
2024-01-20 19:44:35 -0300

    feat: adding categories

--- galery/models.py
+++ galery/models.py
@@ -2,8 +2,17 @@ from django.db import models
 
 
 class Picture(models.Model):
+    
+    CATEGORY_OPTIONS = [
+        ("NEBULOSA","Nebulosa"),
+        ("ESTRELA","Estrela"),
+        ("GALÁXIA","Galáxia"),
+        ("PLANETA","Planeta"),
+    ]
+    
     name = models.CharField(max_length=100, null=False, blank=False)
     credits = models.CharField(max_length=10, null=False, blank=False)
+    category = models.CharField(max_length=100, choices=CATEGORY_OPTIONS, default='')
     description = models.TextField(null=False, blank=False)
     picture = models.CharField(max_length=100, null=False, blank=False)
 
