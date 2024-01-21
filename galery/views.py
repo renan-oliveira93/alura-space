7f0435b5fecf02e97224c6d916685f46ed76da55
Renan de Oliveira <renan_oliveira93@outlook.com.br>
2024-01-21 14:39:53 -0300

    adding search endpoint

--- galery/views.py
+++ galery/views.py
@@ -2,8 +2,16 @@ from django.shortcuts import render, get_object_or_404
 from galery.models import Picture
 
 def index(request):
-    pictures = Picture.objects.all()
+    pictures = Picture.objects.order_by("-picture_date").filter(published=True)
     return render(request, 'galery/index.html', {"cards": pictures})
 def image(request, clicked_picture_id):
     clicked_picture = get_object_or_404(Picture, pk=clicked_picture_id)
     return render(request, 'galery/image.html', {"picture": clicked_picture})
+
+def search(request):
+    pictures = Picture.objects.order_by("-picture_date").filter(published=True)
+    if 'search' in request.GET:
+        search_input_value = request.GET['search']
+        if search_input_value:
+            pictures = pictures.filter(name__icontains=search_input_value)
+    return render(request, "galery/search.html", {"cards": pictures})
