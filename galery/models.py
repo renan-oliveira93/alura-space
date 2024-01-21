b614cc4a95efcdc38466ab7587c396c816692342
Renan de Oliveira <renan_oliveira93@outlook.com.br>
2024-01-21 14:36:38 -0300

    feat: adding published and picture_date

--- galery/models.py
+++ galery/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from datetime import datetime
 
 class Picture(models.Model):
     
@@ -11,10 +11,12 @@ class Picture(models.Model):
     ]
     
     name = models.CharField(max_length=100, null=False, blank=False)
-    credits = models.CharField(max_length=10, null=False, blank=False)
+    credits = models.CharField(max_length=100, null=False, blank=False)
     category = models.CharField(max_length=100, choices=CATEGORY_OPTIONS, default='')
     description = models.TextField(null=False, blank=False)
-    picture = models.CharField(max_length=100, null=False, blank=False)
+    picture = models.ImageField(upload_to="pictures/%Y/%m/%d", blank=True)
+    published = models.BooleanField(default=False)
+    picture_date = models.DateTimeField(default=datetime.now, blank=False)
 
     def __str__(self):
-        return f"Fotografia [name={self.name}]"
+        return self.name
