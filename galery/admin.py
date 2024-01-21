69e2b4c6830cbef8fe8b043b9cb43d56b2830cdf
Renan de Oliveira <renan_oliveira93@outlook.com.br>
2024-01-21 14:35:38 -0300

    feat: adding published category and pagination

--- galery/admin.py
+++ galery/admin.py
@@ -1,9 +1,12 @@
 from django.contrib import admin
 from galery.models import Picture
 
-class picture_list(admin.ModelAdmin):
-    list_display = ("id", "name", "credits")
+class picture_list_view(admin.ModelAdmin):
+    list_display = ("id", "name", "credits", "published")
     list_display_links =("id", "name")
+    list_editable = ("published",)
     search_fields = ("name",)
+    list_filter = ("category","published")
+    list_per_page = 10
 
-admin.site.register(Picture, picture_list)
+admin.site.register(Picture, picture_list_view)
