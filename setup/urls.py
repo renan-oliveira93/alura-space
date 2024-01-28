66f46151946ca0eb034fcc89c605afb0c3d1179d
Renan de Oliveira <renan_oliveira93@outlook.com.br>
2024-01-28 13:26:09 -0300

    feat: create connection to aws bucket

--- setup/urls.py
+++ setup/urls.py
@@ -22,7 +22,12 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('galery.urls')),
-    path('', include('users.urls'))
+    path('', include('apps.galery.urls')),
+    path('', include('apps.users.urls'))
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
